18ce271b8019d8be2c5bd81f077c340ecc6af0c6
Pawel Marcinkowski <marcinkowski.pwl@gmail.com>
2022-10-18 06:56:29 +0200

    JS refactoring

--- js/script.js
+++ js/script.js
@@ -1,58 +1,74 @@
-let formElement = document.querySelector(".js-form");
-let currencyToElement = document.querySelector(".js-currnecyTo");
-let amountElement = document.querySelector(".js-amount");
-let exchangeRateElement = document.querySelector(".js-exchangeRate");
-let resultElement = document.querySelector(".js-result");
+{
+    const formElement = document.querySelector(".js-form");
+    const currencyToElement = document.querySelector(".js-currnecyTo");
 
-let pln = 1;
-let eur = 4.86;
-let usd = 5.09;
-let gbp = 5.53;
-let rub = 0.04;
-let chf = 5.02;
+    const pln = 1;
+    const eur = 4.86;
+    const usd = 5.09;
+    const gbp = 5.53;
+    const rub = 0.04;
+    const chf = 5.02;
 
-formElement.addEventListener("input", (event) => {
-    event.preventDefault();
+    const chooseCurrency = (currencyToElement) => {
+        switch (currencyToElement.value) {
+            case "PLN":
+                return exchangeRateElement.innerText = pln;
 
-    switch (currencyToElement.value) {
-        case "PLN":
-            exchangeRateElement.innerText = pln;
-            break;
-        case "EUR":
-            exchangeRateElement.innerText = eur;
-            break;
-        case "USD":
-            exchangeRateElement.innerText = usd;
-            break;
-        case "GBP":
-            exchangeRateElement.innerText = gbp;
-            break;
-        case "RUB":
-            exchangeRateElement.innerText = rub;
-            break;
-        default:
-            exchangeRateElement.innerText = chf;
+            case "EUR":
+                return exchangeRateElement.innerText = eur;
+
+            case "USD":
+                return exchangeRateElement.innerText = usd;
+
+            case "GBP":
+                return exchangeRateElement.innerText = gbp;
+
+            case "RUB":
+                return exchangeRateElement.innerText = rub;
+
+            default:
+                return exchangeRateElement.innerText = chf;
+        }
     }
-})
+    const exchangeRateElement = document.querySelector(".js-exchangeRate");
+
+    formElement.addEventListener("input", (event) => {
+        event.preventDefault();
+
+        let showCurrency = chooseCurrency(currencyToElement);
+
+        exchangeRateElement.innerText = showCurrency;
+    })
 
 
-formElement.addEventListener("submit", (rate) => {
-    rate.preventDefault();
+    const amountElement = document.querySelector(".js-amount");
+    const resultElement = document.querySelector(".js-result");
 
-    if (currencyToElement.value === "PLN") {
-        result = amountElement.value / pln;
-    } else if (currencyToElement.value === "EUR") {
-        result = amountElement.value / eur;
-    } else if (currencyToElement.value === "USD") {
-        result = amountElement.value / usd;
-    } else if (currencyToElement.value === "GBP") {
-        result = amountElement.value / gbp;
-    } else if (currencyToElement.value === "RUB") {
-        result = amountElement.value / rub;
-    } else {
-        result = amountElement.value / chf;
+    const calculateResult = (currencyToElement, amountElement) => {
+
+        switch (currencyToElement.value) {
+            case "PLN":
+                return amountElement.value / pln;
+
+            case "USD":
+                return amountElement.value / usd;
+
+            case "GBP":
+                return amountElement.value / gbp;
+
+            case "RUB":
+                return amountElement.value / rub;
+
+            default:
+                return amountElement.value / chf;
+        }
     }
 
-    resultElement.innerText = `You will get ${result.toFixed(2)}`;
+    formElement.addEventListener("submit", (rate) => {
+        rate.preventDefault();
 
-})
+        let result = calculateResult(currencyToElement, amountElement);
+
+        resultElement.innerText = `You will get ${result.toFixed(2)}`;
+    })
+}
